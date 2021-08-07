b36e347f52edd0419c90d652449143288f77bb43
Serhii Soboliev <sergiy.sobolev@gmail.com>
2021-08-08 00:29:13 +0300

    safe data thread

--- src/main/java/org/sbk/datarace/DataRace.java
+++ src/main/java/org/sbk/datarace/DataRace.java
@@ -3,8 +3,8 @@ package org.sbk.datarace;
 public class DataRace {
     public static void main(String[] args) {
         Runnable runnable = new TestRunnable();
-        Thread t1 = new Thread(runnable);
-        Thread t2 = new Thread(runnable);
+        Thread t1 = new Thread(runnable, "Thread 1");
+        Thread t2 = new Thread(runnable, "Thread 2");
         t1.start();
         t2.start();
     }
--- src/main/java/org/sbk/datarace/NoDataRace.java
+++ src/main/java/org/sbk/datarace/NoDataRace.java
@@ -2,8 +2,8 @@ package org.sbk.datarace;
 
 public class NoDataRace {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new TestRunnable());
-        Thread t2 = new Thread(new TestRunnable());
+        Thread t1 = new Thread(new TestRunnable(), "Thread 1");
+        Thread t2 = new Thread(new TestRunnable(), "Thread 2");
         t1.start();
         t2.start();
     }
