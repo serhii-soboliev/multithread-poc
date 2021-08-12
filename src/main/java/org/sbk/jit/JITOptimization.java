862f9f57e0c2eb85bbc079dc02c6acbec065d311
Serhii Soboliev <sergiy.sobolev@gmail.com>
2021-08-12 22:55:06 +0300

    JIT Optimization refactor

--- src/main/java/org/sbk/jit/JITOptimization.java
+++ src/main/java/org/sbk/jit/JITOptimization.java
@@ -2,31 +2,31 @@ package org.sbk.jit;
 
 public class JITOptimization {
 
-    private static boolean flag = true;
+    private static boolean enabled = true;
 
     public static void main(String[] args) throws InterruptedException {
         new Thread(JITOptimization::process).start();
-        Thread.sleep(1000);
-        new Thread(JITOptimization::terminate).start();
+        Thread.sleep(5);
+        new Thread(JITOptimization::disableProcessing).start();
     }
 
-    private static void terminate() {
-        flag = false;
-        System.out.println("Flag has been set to False");
+    private static void disableProcessing() {
+        enabled = false;
+        System.out.println("Processing disable called");
     }
 
-    private static boolean getFlag() {
-        return flag;
+    private static boolean isEnabled() {
+        return enabled;
     }
 
     private static void process() {
         boolean printed = false;
-        while (getFlag()) {
+        while (isEnabled()) {
             if(!printed) {
-                System.out.println("I'm inside the loop!");
+                System.out.println("Processing started");
                 printed = true;
             }
         }
-        System.out.println("I'm outside the loop");
+        System.out.println("Processing finished");
     }
 }
