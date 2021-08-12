7024a300c75d77e4c9dc89c60cff8df6618ee28b
Serhii Soboliev <sergiy.sobolev@gmail.com>
2021-08-12 23:26:20 +0300

    JIT Optimization move to static

--- src/main/java/org/sbk/jit/JITOptimization.java
+++ src/main/java/org/sbk/jit/JITOptimization.java
@@ -2,27 +2,37 @@ package org.sbk.jit;
 
 public class JITOptimization {
 
-    private boolean enabled = true;
+    private static boolean enabled = true;
 
     public static void main(String[] args) throws InterruptedException {
-        var jitOptimization = new JITOptimization();
-        new Thread(jitOptimization::process).start();
-        Thread.sleep(5);
-        new Thread(jitOptimization::disableProcessing).start();
+        new Thread(JITOptimization::process).start();
+        Thread.sleep(2);
+        new Thread(JITOptimization::disableProcessing).start();
     }
 
-    private void disableProcessing() {
+    private static void disableProcessing() {
         enabled = false;
         System.out.println("Processing disable called");
     }
 
-    private boolean isEnabled() {
+    private static boolean isEnabled() {
         return enabled;
     }
 
-    private void process() {
+    private static void process() {
         boolean printed = false;
         while (isEnabled()) {
+            if(!printed) {
+                System.out.println("Processing started");
+                printed = true;
+            }
+        }
+        System.out.println("Processing finished");
+    }
+
+    private static void process2() {
+        boolean printed = isEnabled();
+        while (printed) {
             if(!printed) {
                 System.out.println("Processing started");
                 printed = true;
